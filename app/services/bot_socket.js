babfe98072fc9c0e3b97562e3d5ea9da67868a69
kjw <kishe56@gmail.com>
2018-05-11 16:24:36 +0900

    bot_socket 메시지 정상적으로 렌더 되도록 수정 - MessageFactory 의 prepareBotMessageRow 수정 - default_socket 의 createdToken 이벤트 콜백에서 loadRoomList 호출 조건 추가

--- app/services/bot_socket.js
+++ app/services/bot_socket.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function connectToBot(id, token, url, renderer) {
+function connectToBot(args, token, url, renderer) {
   const socket = require('socket.io-client')(url,{
     transports: ['websocket'],
     query:{
-      id:id,
-      nickName:'kjw',
+      id:args.id,
+      nickName:args.name,
       authOrg:'facebook',
       token:token
     }
@@ -13,17 +13,17 @@ function connectToBot(id, token, url, renderer) {
   socket.on('connect',()=>{
     console.log('bot socket connected');
   });
-  // socket.on('error',(message)=>{
-  //   console.log(message.statusCode);
-  //   console.log(message);
-  // });
-  // socket.on('news-bot',(message)=>{
-  //   console.log(message);
-  //   renderer.renderMessage(message.message,renderer.messageType.ANOTHER_MESSAGE,socket.id);
-  // });
-  // socket.on('message-public', function (data) {
-  //   renderer.renderMessage(data.message,renderer.messageType.ANOTHER_MESSAGE,socket.id);
-  // });
+  socket.on('error',(message)=>{
+    console.log(message.statusCode);
+    console.log(message);
+  });
+  socket.on('news-bot',(message)=>{
+    console.log(message);
+    renderer.renderMessage(message.createdMessage,renderer.messageType.ANOTHER_MESSAGE,socket.id);
+  });
+  socket.on('message-public', function (message) {
+    renderer.renderMessage(message.createdMessage,renderer.messageType.ANOTHER_MESSAGE,socket.id);
+  });
   return socket;
 };
 
