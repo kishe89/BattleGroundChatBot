dbf4d289bbb2bd7330a981d53415bc6a60dc98eb
kjw <kishe56@gmail.com>
2018-05-20 16:18:30 +0900

    test/PromiseCancel.js 작성 ### test/PromiseCancel.js
    - 취소 가능한 프로미스 구현을 위한 테스트 케이스
    - Cancelable Promise 구현 테스트
    - Cancelable Promise 반환하는 createCancleAblePromise 함수 작성

--- test/PromiseCancel.js
+++ test/PromiseCancel.js
@@ -18,7 +18,7 @@ describe('Promise Cancel',()=>{
     const task = new Promise((resolve, reject)=>{
       let index;
       try{
-        for( index = 0; index < 100000; index++){
+        for( index = 0; index < 10; index++){
           console.log(index);
         }
       }catch(e) {
@@ -57,7 +57,7 @@ describe('Promise Cancel',()=>{
           return reject(event);
         });
         try{
-          for( index = 0; index < 100000; index++){
+          for( index = 0; index < 10; index++){
             console.log(index);
           }
         }catch(e) {
@@ -81,54 +81,84 @@ describe('Promise Cancel',()=>{
     });
     MyEmitter.emit('reject','cancel task');
   });
+  it('setTimeout test',(done)=>{
+    new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        for(let i =0 ; i <100 ; i++){
+          console.log(i);
+        }
+        resolve();
+      },1000)
+    }).then(()=>{
+      done();
+    });
+
+  });
   it('virtual click event And Cancel test',(done)=>{
 
     let agoTask = {
-      target : '',
-      isExcuted : false,
-      task : undefined
+      target : undefined,
+      task:{}
     };
+    function createCancleAblePromise(element) {
+      let reject = undefined;
+      let isExcuted = false;
+      const promise = new Promise((PromiseResolve, PromiseReject)=>{
 
+        reject = ()=>{
+          PromiseReject('cancel Task target is : '+element);
+        };
+        setTimeout(()=>{
+          let index;
+          for(index = 0 ; index < 10; index++){
+            console.log(index);
+          }
+          isExcuted = true;
+          return PromiseResolve('complete' + index);
+        },1000);
+
+      });
+      return {
+        isExcuted : isExcuted,
+        reject : reject,
+        promise: promise
+      };
+    }
     MyEmitter.on('click',(element)=>{
       console.log(element);
-      const TaskObject = {
-        emitter : new EventEmitter(),
-        task : function () {
-          agoTask.target = element;
-          agoTask.task = TaskObject;
-          return new Promise((resolve, reject)=>{
-            let index;
-            this.emitter.on('reject',(event)=>{
-              console.log('reject event called with '+event);
-              return reject(event);
-            });
-            try{
-              for( index = 0; index < 100; index++){
-                console.log(index);
-              }
-            }catch(e) {
-              reject(e);
-            }
+      if(agoTask.task.isExcuted){
+        // 이전 작업이 실행된 상태 새로운 작업 그냥 생성하면됨
+        console.log('실행되고 나서 새로운 작업생성');
+        agoTask.target = element;
+        agoTask.task = createCancleAblePromise(element);
 
-            return resolve(index);
-          });
-        }
-      };
-      if(agoTask.target !== element){
-        console.log('come');
-        if(!agoTask.isExcuted && agoTask.task !== undefined){
-          agoTask.task.emitter.emit('reject',element);
+      }else{
+        if(agoTask.target){
+          //실행안된 상태로 이전 작업 취소해야함
+          console.log('취소할 작업 '+JSON.stringify(agoTask));
+          agoTask.task.isExcuted = true;
+          agoTask.task.reject('client busy so cancel task '+agoTask.target);
+
+          agoTask.target = element;
+          agoTask.task = createCancleAblePromise(element);
+
+        }else{
+          // 맨 처음 실행시 태스크가 없을 때
+          agoTask.target = element;
+          agoTask.task = createCancleAblePromise(element);
+          console.log('target 없어서 생성된 작업'+JSON.stringify(agoTask));
         }
       }
-      TaskObject.task().then((value)=>{
-        agoTask.isExcuted = true;
-        console.log('Promise resolved '+element);
-        done();
-      }).catch((e)=>{
-        agoTask.isExcuted = true;
-        console.log('rejected = '+e);
-        done(e);
-      });
+
+      agoTask.task.promise
+        .then((v)=>{
+          console.log(v + JSON.stringify(agoTask));
+          done();
+        })
+        .catch((e)=>{
+          console.log(e);
+          // done();
+        });
     });
     MyEmitter.emit('click','srcElement1');
     console.log('click ----------');
